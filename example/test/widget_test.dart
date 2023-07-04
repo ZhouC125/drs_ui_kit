483ecee57e6d03be7f6097109cd103da41244d90
achen <1216175270@qq.com>
2023-07-04 09:45:33 +0800

    修复test文件的错误

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,14 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:drs_ui_kit_example/main.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
     // Verify that platform version is retrieved.
+
     expect(
       find.byWidgetPredicate(
         (Widget widget) => widget is Text &&
